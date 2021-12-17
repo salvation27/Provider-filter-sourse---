5b204d0e7f1ec01768c036d01679db2bc84e0af8
Salvation27 <nikolayzalipykin@gmail.com>
2021-12-17 23:34:30 +0200

    qq

--- src/components/Section/Context/Products/Buttons.js
+++ src/components/Section/Context/Products/Buttons.js
@@ -2,33 +2,30 @@ import React from 'react'
 
 export default function Buttons({handleBtns}) {
   return (
-    <div className='btn_wrapper'>
-      <button
-      onClick={handleBtns}
-      value='All'
-      >
-      All</button>
+    <div className="btn_wrapper">
+      <button onClick={handleBtns} value="All">
+        All
+      </button>
 
-        <button
-        onClick={handleBtns}
-        value='react'
-        >
-        React</button>
-      
-        <button 
-      onClick={handleBtns}
-      value='javascript'>Javascript</button>
-      <button 
-      onClick={handleBtns}
-      value='js_l'>Javascript-урок</button>
-        <button 
-      onClick={handleBtns}
-      value='js_task'>Javascript-задачки</button>
-      <button 
-      onClick={handleBtns}
-      value='css'>Css</button>
+      <button onClick={handleBtns} value="react">
+        React
+      </button>
 
-
+      <button onClick={handleBtns} value="javascript">
+        Javascript
+      </button>
+      <button onClick={handleBtns} value="js_l">
+        Javascript-урок
+      </button>
+      <button onClick={handleBtns} value="js_task">
+        Javascript-задачки
+      </button>
+      <button onClick={handleBtns} value="css">
+        Css
+      </button>
+      <button onClick={handleBtns} value="css2222">
+        Css2222
+      </button>
     </div>
-  )
+  );
 }
